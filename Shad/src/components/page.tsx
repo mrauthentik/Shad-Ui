d6635509353305d08a04f23fceb811a5e361f9f9
mrauthentik <umokeuchenna2020@gmail.com>
2025-03-10 18:44:09 +0100

    refactor: enhance header styling and improve separator usage in page component

--- Shad/src/components/page.tsx
+++ Shad/src/components/page.tsx
@@ -20,10 +20,10 @@ const page = () => {
       <SidebarProvider >
         <AppSidebar />
         <SidebarInset> 
-            <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height]">
+            <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
                 <div className="flex items-center gap-2 px-4">
-                 <SidebarTrigger className="-ml-1">
-                    <Separator orientation="vertical">
+                 <SidebarTrigger className="-ml-1" />
+                    <Separator orientation="vertical" className="mr-2 h-4"/>
                     <Breadcrumb >
                      <BreadcrumbList>
                       <BreadcrumbItem className="hidden md:block">
@@ -37,9 +37,8 @@ const page = () => {
                       </BreadcrumbItem>
                      </BreadcrumbList>
                     </Breadcrumb>
-                    </Separator>
+                    <Separator orientation="vertical" />
                     <h2>Welcome</h2>
-                 </SidebarTrigger>
                 </div>
             </header>
             <div className="flex flex-1 flex-col gap-4 p-4 pt-0 bg-gray-800">
